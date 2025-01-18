6f1800045288c7644fde9bc4245d9f89cdcc3fd4
Shinichiro-Nuno <nuno1216shin@gmail.com>
2025-01-18 21:02:47 +0900

    Chakra UI V2インストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
   console.log(process.env.VITE_SECRET_KEY);
   return (
     <>
       <p>Vite:{process.env.VITE_SECRET_KEY}</p>
+      <Button>Click</Button>
     </>
   );
 }
